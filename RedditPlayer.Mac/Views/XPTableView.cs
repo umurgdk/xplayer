cae108daeb546c59960fc690c3816332cf3744a1
umurgdk <umurgdk@gmail.com>
2016-07-10 14:56:31 +0700

    update

---
+++ RedditPlayer.Mac/Views/XPTableView.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using AppKit;
+using CoreGraphics;
+using Foundation;
+namespace RedditPlayer.Mac.Views
+{
+    [Register ("XPTableView")]
+    public class XPTableView : NSTableView
+    {
+        public XPTableView ()
+        {
+        }
+
+        public XPTableView (NSCoder coder) : base (coder)
+        {
+        }
+
+        public XPTableView (NSObjectFlag t) : base (t)
+        {
+        }
+
+        public XPTableView (IntPtr handle) : base (handle)
+        {
+        }
+
+        public XPTableView (CGRect frameRect) : base (frameRect)
+        {
+        }
+
+        public override CoreGraphics.CGSize IntrinsicContentSize
+        {
+            get
+            {
+                var height = Math.Max (RowCount, 1) * RowHeight;
+
+                return new CGSize (1, height);
+            }
+        }
+    }
+}
+
